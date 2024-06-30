bd8b75e57850942c69c682437d24bfd51528c2f0
Sanket Parsewar <sanket.parsewar97@gmail.com>
2024-06-30 13:21:05 +0530

    initial commit

---
+++ assets2/js/chatbot.js
@@ -0,0 +1,2 @@
+(function (w, d) { w.CollectId = "645dc6f01b0491e28f6192f8"; var h = d.head || d.getElementsByTagName("head")[0]; var s = d.createElement("script"); s.setAttribute("type", "text/javascript"); s.async = true; s.setAttribute("src", "https://collectcdn.com/launcher.js"); h.appendChild(s); })(window, document);
+
